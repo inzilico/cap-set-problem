18ea996ea61fc60923f965c44f92a20ac2467f53
Gennady Khvorykh <info@inzilico.com>
2023-08-14 08:10:17 +0300

    script is finished

--- find-cap-sets-04.pl
+++ find-cap-sets-04.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 ## Find cap sets
-## File with 3-card combinations should be labeled by {0 ,1} having 4 columns.
+## File with 3-card combinations should be labeled by {0,1} having 4 columns.
 ## To check the possibility for the open cards to form sets,
-## 2-card combinations among open cards are calculate and compared with all cards in deck. 
+## 2-card combinations among open cards are calculated and compared with all cards in deck. 
 ## The deck cards forming set with the given 2-card combination are removed. 
 ## Author: Gennady Khvorykh, info@inzilico.com
 
@@ -21,22 +21,11 @@ die "Wrong number of arguments" if ($#ARGV + 1 != 3);
 die "$f1 doesn't exist or empty" unless -s $f1;
 die "$f2 doesn't exist or empty" unless -s $f2;
 
+# Load cards
+my ($cards, $ind)	 = load_cards($f2);
+
 # Open file with labeled 3-card combinations
 open (my $fh1, '<', $f1) or die "Failed to open $f1";
-
-# Open file with cards 
-open (my $fh2, '<', $f2) or die "Failed to open $f2";
-
-# Load cards
-my @cards = <$fh2>;
-chomp @cards;
-my $size = @cards;
-print "Number of cards: $size\n";
-# Let cards start with index 1
-unshift @cards, 0;
-
-# Initiate card index
-my @ind = 1..$size;
 
 # Open file to save output
 open (my $fh3, '>', $f3) or die "Failed to open $f3";
@@ -55,29 +44,43 @@ while (<$fh1>) {
 	# Skip if set
 	next if $l == 1;
 	# Initiate deck
-	my @deck = diff(\@ind, \@oc);
+	my @deck = diff($ind, \@oc);
 	# Generate 2-card combinations from open cards
 	my @cmbs = combinations(\@oc, 2);
 	# Process deck until it has cards
 	until (@deck == 0) {
 		# Loop over 2-card combinations
-		foreach my $cmb (@cmbs) {
-			# Check sets with deck cards left
-			
+		foreach (@cmbs) {
+			my @cmb = @$_;
+			my %seen = ();
+			# Loop over deck cards 
+			foreach my $card (@deck) {
+				# Make three cards for test
+				my @test = @cmb;
+				push @test, $card;
+				# Test for set 
+				my $res = is_set(@{ $cards }[@test]);
+				# Mark bad cards
+				$seen{$card} = 1 if $res == 1;
+			}
 			# Remove bad cards from deck
-			
-			# Check cards are left in deck 
+			my @deck_new;
+			foreach my $item (@deck) {
+				push @deck_new, $item unless($seen{$item}); 
+			}
+			# Check cards are left in deck
+			last if (@deck_new == 0);
+			# Update deck
+			@deck = @deck_new;
 		}
-		
-
+		# Check if deck still has cards
+		last if (@deck == 0);
 		# Take card from deck
-		my $card = shift @deck;
-		# Add card to open cards if it doesn't make sets with open cards
-		# and update 2-card combinations
-		unless (sets(\@cmbs, $card, \@cards)) {
-			push @oc, $card;
-			@cmbs = combinations(\@oc, 2);
-		}
+		my $crd = shift @deck;
+		# Add to open cards
+		push @oc, $crd;		
+		# Generate new 2-card combinations
+		@cmbs = combinations(\@oc, 2);
 	}
 
 	# Show cap
@@ -89,12 +92,29 @@ print "The number of non-sets processed: $n\n";
 
 # Close files
 close $fh1;
-close $fh2;
 close $fh3;
 
 exit 0;
 
 # === subs ===
+
+sub load_cards {
+	my $f = $_[0];
+	# Open file with cards 
+	open (my $fh, '<', $f) or die "Failed to open $f";
+	# Load cards
+	my @cards = <$fh>;
+	chomp @cards;
+	my $size = @cards;
+	print "Number of cards: $size\n";
+	# Let cards start with index 1
+	unshift @cards, 0;
+	# Initiate card index
+	my @ind = 1..$size;
+	close $fh;
+	return \@cards, \@ind;
+}
+
 
 sub is_set {
 	my @arg = @_;
